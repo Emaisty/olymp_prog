b4d4506011706b78a73cab64e741418bce550d83
Emaisty <maxgusevfff@yandex.ru>
2019-01-15 01:38:02 +0300

    add file

--- main.cpp
+++ main.cpp
@@ -5,55 +5,57 @@
 
 using namespace std;
 
-int color[200000], ma = 0;
-vector<vector<int>> arr(200000, vector<int>());
-
-void cl(int a, vector<int> &err) {
-    for (int i = 0; i < arr[a].size(); ++i) {
-        err.push_back(color[arr[a][i]]);
-    }
+bool cmp(vector<int> a, vector<int> b) {
+    return a[1] < b[1];
 }
-
-void df(int a) {
-    vector<int> err;
-    for (int i = 0; i < arr[a].size(); ++i) {
-        err.push_back(color[arr[a][i]]);
-        cl(arr[a][i], err);
-    }
-    sort(err.begin(), err.end());
-    int u = 1;
-    for (int i = 0; i < err.size(); ++i) {
-        if (err[i] == u)
-            u++;
-    }
-    if (u > ma)
-        ma = u;
-    color[a] = u;
-}
-
 
 int main() {
-    int n;
-    cin >> n;
-    for (int i = 0, x, y; i < n - 1; ++i) {
-        cin >> x >> y;
-        x--;
-        y--;
-        arr[x].push_back(y);
-        arr[y].push_back(x);
+    int a, b, s1 = 0, s2 = 0, c1 = 0, c2 = 0;
+    cin >> a >> b;
+    vector<vector<int>> arr(a + b, vector<int>(4));
+    for (int i = 0, x, y = 0; i < a; ++i) {
+        cin >> x;
+        arr[i][0] = y;
+        arr[i][1] = y + x;
+        y += x;
+        arr[i][2] = 1;
+
     }
-    for (int i = 0; i < 200000; ++i) {
-        color[i] = 0;
+    for (int i = 0, x, y = 0; i < b; ++i) {
+        cin >> x;
+        arr[i + a][0] = y;
+        arr[i + a][1] = y + x;
+        y += x;
+        arr[i][2] = 2;
     }
-    color[0] = 1;
-    df(2);
-    for (int i = 1; i < n; ++i) {
-        if (i != 2)
-            df(i);
+
+    sort(arr.begin(), arr.end(), cmp);
+    vector<int> r, t;
+    for (int i = 0, x = 0; i < a + b; ++i) {
+        if (arr[i][2] == 2)
+            s1++;
+        else
+            s2++;
+        if (arr[i][0] >= x) {
+            if (arr[i][2] == 2) {
+                c1++;
+                r.push_back(s1);
+            } else {
+                c2++;
+                t.push_back(s2);
+            }
+            x = arr[i][1];
+        }
+
     }
-    cout << ma << endl;
-    for (int i = 0; i < n; ++i) {
-        cout << color[i] << ' ';
+    cout << c1 << ' ' << c2 << endl;
+    for (int i = 0; i < c1; ++i) {
+        cout << r[i] << ' ';
     }
+    cout << endl;
+    for (int i = 0; i < c2; ++i) {
+        cout << t[i] << ' ';
+    }
+
     return EXIT_SUCCESS;
 }
