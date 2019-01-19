40513362ab195b03fe25e0d2527ddda3abd99b34
Emaisty <38889783+Emaisty@users.noreply.github.com>
2019-01-19 15:00:42 +0300

    skan line algo

--- skan_line_algo.cpp
+++ skan_line_algo.cpp
@@ -5,57 +5,58 @@
 
 using namespace std;
 
-bool cmp(vector<int> a, vector<int> b) {
-    return a[1] < b[1];
+bool c(vector<long long> a, vector<long long> b) {
+    if (b[1] == a[1]) {
+        return a[0] < b[0];
+    } else {
+        return a[1] < b[1];
+    }
 }
 
 int main() {
-    int a, b, s1 = 0, s2 = 0, c1 = 0, c2 = 0;
+    int a, b, c1 = 0, c2 = 0;
     cin >> a >> b;
-    vector<vector<int>> arr(a + b, vector<int>(4));
-    for (int i = 0, x, y = 0; i < a; ++i) {
+    vector<vector<long long>> arr(a + b, vector<long long>(4));
+    for (long long i = 0, x, t = 0; i < a; ++i) {
         cin >> x;
-        arr[i][0] = y;
-        arr[i][1] = y + x;
-        y += x;
+        arr[i][0] = t;
+        t += x;
+        arr[i][1] = t;
         arr[i][2] = 1;
-
+        arr[i][3] = i;
     }
-    for (int i = 0, x, y = 0; i < b; ++i) {
+    for (long long j = a, x, t = 0; j < a + b; ++j) {
         cin >> x;
-        arr[i + a][0] = y;
-        arr[i + a][1] = y + x;
-        y += x;
-        arr[i][2] = 2;
+        arr[j][0] = t;
+        t += x;
+        arr[j][1] = t;
+        arr[j][2] = 2;
+        arr[j][3] = j - a;
     }
-
-    sort(arr.begin(), arr.end(), cmp);
-    vector<int> r, t;
-    for (int i = 0, x = 0; i < a + b; ++i) {
-        if (arr[i][2] == 2)
-            s1++;
-        else
-            s2++;
-        if (arr[i][0] >= x) {
-            if (arr[i][2] == 2) {
+    sort(arr.begin(), arr.end(), c);
+    long long timer = 0;
+    vector<int> x, y;
+    for (int i = 0; i < a + b; ++i) {
+        if (timer <= arr[i][0]) {
+            timer = arr[i][1];
+            if (arr[i][2] == 1) {
+                x.push_back(arr[i][3]);
                 c1++;
-                r.push_back(s1);
             } else {
+                y.push_back(arr[i][3]);
                 c2++;
-                t.push_back(s2);
             }
-            x = arr[i][1];
         }
-
     }
     cout << c1 << ' ' << c2 << endl;
-    for (int i = 0; i < c1; ++i) {
-        cout << r[i] << ' ';
+    if (c1 != 0) {
+        for (int i = 0; i < c1; ++i) {
+            cout << x[i] + 1 << ' ';
+        }
+        cout << endl;
     }
-    cout << endl;
     for (int i = 0; i < c2; ++i) {
-        cout << t[i] << ' ';
+        cout << y[i] + 1 << ' ';
     }
-
     return EXIT_SUCCESS;
 }
